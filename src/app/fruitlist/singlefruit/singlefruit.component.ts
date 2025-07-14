2df88dc7b7255d1bdbfb9ce78a2c5e26b287ae78
Marcel Göhn <marcelgoehn@googlemail.com>
2025-07-14 12:48:07 +0200

    Added @input, @output and @injectable to add comments and to have the data in a central spot

--- src/app/fruitlist/singlefruit/singlefruit.component.ts
+++ src/app/fruitlist/singlefruit/singlefruit.component.ts
@@ -1,9 +1,10 @@
-import { Component, Input } from '@angular/core';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-singlefruit',
   standalone: true,
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './singlefruit.component.html',
   styleUrl: './singlefruit.component.scss'
 })
@@ -16,4 +17,13 @@ export class SinglefruitComponent {
       stars: 2.3,
       reviews: [{ name: "Kevin W.", text: "ist lecker" }, { name: "Arne P.", text: "nicht so meins" }],
     };
+
+    inputData: string = "";
+
+    @Output() fruitName = new EventEmitter<string>();
+
+    sendInputData() {
+      this.fruitName.emit(this.inputData);
+      this.inputData = "";
+    }
 }
